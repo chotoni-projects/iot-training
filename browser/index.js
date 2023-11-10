67d89ab0b0161c9544f0078e327dd8fd1174dd83
Rifqi Thomi <rifqi.arti.rt@gmail.com>
2023-11-10 22:06:12 +0700

    Update index.js

--- browser/index.js
+++ browser/index.js
@@ -19,9 +19,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         // Once a connection has been made, make a subscription and send a message.
         console.log("onConnect");
         client.subscribe("capacity");
-        // message = new Paho.MQTT.Message("Hello");
-        // message.destinationName = "World";
-        // client.send(message);
     }
 
     // called when the client loses its connection
